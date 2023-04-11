33130edc9ef1c4a693b7d38dd1c7e8b788a478eb
Henrik Larsson <henriklarsson@Henriks-MacBook-Pro.local>
2023-04-11 19:49:07 +0200

    Initial baseline for site

---
+++ build-site.el
@@ -0,0 +1,40 @@
+;; Set the package installation directory so that packages aren't stored in the
+;; ~/.emacs.d/elpa path.
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+;; Initialize the package system
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Install dependencies
+(package-install 'htmlize)
+
+(require 'ox-publish)
+
+(setq org-html-validation-link nil            ;; Don't show validation link
+      org-html-head-include-scripts nil       ;; Use our own scripts
+      org-html-head-include-default-style nil ;; Use our own styles
+      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+
+
+(setq org-publish-project-alist
+      (list
+       (list "henke-larsson-ab-org-site"
+	     :recursive t
+	     :base-directory "./content" ; where to put all org files
+	     :publishing-directory "./public"
+	     :publishing-function 'org-html-publish-to-html
+	     :with-author nil
+	     :with-creator t
+	     :with-toc t
+	     :section-numbers nil
+	     :time-stamp-file nil
+	     )))
+
+(org-publish-all t)
+
+(message "Build complete!")
